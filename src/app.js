277d0ce04a2e928c09d1abf67d03ff4790ee8701
sahil0079 <114945558+sahil0079@users.noreply.github.com>
2023-12-09 22:37:21 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,22 +1,31 @@
 const http = require('http');
+const url = require('url');
 
 const server = http.createServer((req, res) => {
-  if (req.method === 'POST') {
-    const chunks = [];
+  const parsedUrl = url.parse(req.url, true);
 
-    req.on('data', chunk => {
-      const buf = Buffer.from(chunk);
-      const str = buf.toString();
-      chunks.push(str);
-      const obj = JSON.parse(chunks)
-      const value1 = obj.num1;
-      const value2 = obj.num2;
+  console.log('parsedUrl', parsedUrl)
+  const { pathname, query } = parsedUrl;
+  if (req.method === 'GET' && pathname === '/') {
+    const num1 = parseInt(query.num1);
+    const num2 = parseInt(query.num2);
 
-      // Write code here to calculate power of a number
-      
-    });
+    if (isNaN(num1) || isNaN(num2) || num1 <= 0 || num2 < 0) {
+      res.writeHead(400, { 'Content-Type': 'text/plain' });
+      res.end('Invalid input. Please provide valid positive integers');
+    } else if (num1 === 0 || num2 === 0) {
+      res.writeHead(404, { 'Content-Type': 'text/plain' });
+      res.end('The operation cannot be performed');
+    } else {
+      //calculate the power 
+      const result = Math.pow(num1, num2);
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
+      res.end(`The result is ${result}`);
     }
+
+  }
+
 });
 
+
 module.exports = server;
-      
